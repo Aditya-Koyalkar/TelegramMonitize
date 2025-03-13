183cc2c578879a000af6f49bca5779fcc48e0a67
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-03-13 22:04:51 +0530

    GROUP handling using bot

--- backend/src/bot/index.ts
+++ backend/src/bot/index.ts
@@ -1,3 +1,4 @@
+import { assignGroup } from "./handler/assign.handler.js";
 import { sendMessage } from "./handler/message.handler.js";
 import { rulesHandler } from "./handler/rules.handler.js";
 import { startHandler } from "./handler/start.handler.js";
@@ -6,4 +7,5 @@ export const initBot = () => {
   startHandler();
   sendMessage();
   rulesHandler();
+  assignGroup();
 };
