1ab9d97f8cc051d74b5403c0c65a51e35525f0cc
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-03-20 23:17:57 +0530

    getting the stats api

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono";
 import authRoutes from "./auth/auth.js";
+import statsRoutes from "./stats/stats.route.js";
 const routes = new Hono();
 
 routes.route("/v1/user", authRoutes);
-
+routes.route("/v1/dashboard", statsRoutes);
 export default routes;
---
+++ backend/src/routes/stats/stats.route.ts
@@ -0,0 +1,55 @@
+import { Hono } from "hono";
+import type { AuthSession } from "../../lib/better-auth/auth-types.js";
+import { Group } from "../../lib/database/models/group.model.js";
+import { Transaction } from "../../lib/database/models/transaction.model.js";
+import { Customer } from "../../lib/database/models/customer.model.js";
+
+const statsRoutes = new Hono<AuthSession>();
+
+statsRoutes.get("/overview", async (c) => {
+  const user = c.get("user")!;
+  const last30Day = new Date();
+  const last7Day = new Date();
+  last30Day.setDate(last30Day.getDate() - 30);
+  last30Day.setDate(last7Day.getDate() - 7);
+  const groups = await Group.find({
+    owner: user.id,
+    createdAt: {
+      $gte: last30Day,
+    },
+  });
+  const earnings = groups.reduce((sum, grp) => {
+    return sum + grp.revenue;
+  }, 0);
+  const transactionCount = await Transaction.find({
+    owner: user.id,
+    createdAt: { $gte: last7Day },
+  }).countDocuments();
+  const transactionDetails = await Transaction.find({
+    owner: user.id,
+    createdAt: { $gte: last7Day },
+  }).sort({
+    createdAt: -1,
+  });
+  const customerCount = await Customer.findOne({
+    owner: user.id,
+    createdAt: { $gte: last7Day },
+  }).countDocuments();
+
+  const customerDetails = await Customer.find({
+    owner: user.id,
+    createdAt: { $gte: last7Day },
+  }).sort({ createdAt: -1 });
+
+  const result = {
+    earnings: earnings || 0,
+    totalCustomers: customerCount || 0,
+    customerDetails: customerDetails || [],
+    totalTransactions: transactionCount || 0,
+    transactionDetails: transactionDetails || [],
+  };
+
+  return c.json({ status: true, message: "", result }, 200);
+});
+
+export default statsRoutes;
