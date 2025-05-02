f4b6c2ba518b820653b7d19371802d37f1290311
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-05-02 20:55:08 +0530

    fixes

--- backend/src/lib/better-auth/auth.ts
+++ backend/src/lib/better-auth/auth.ts
@@ -19,6 +19,7 @@ export const auth = betterAuth({
     defaultCookieAttributes: {
       sameSite: "none",
       secure: true,
+      partitioned: true,
     },
     crossSubDomainCookies: {
       enabled: true,
