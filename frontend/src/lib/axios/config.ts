efb415d546f26d33db366de11eb2e3568b163354
KoyalkarAditya <koyalkaraditya123@gmail.com>
2025-05-02 20:26:30 +0530

    fix axios error

--- frontend/src/lib/axios/config.ts
+++ frontend/src/lib/axios/config.ts
@@ -1,12 +1,20 @@
 import { SERVER_DOMAIN } from "@/utils/env";
 import axios from "axios";
+import { cookies } from "next/headers";
+const cookieHeader = (await cookies()).toString();
 
 export const axiosBaseInstance = axios.create({
   baseURL: `${SERVER_DOMAIN}/api/v1`, // Backend server URL
   withCredentials: true, // To send cookies with requests
+  headers: {
+    Cookie: cookieHeader,
+  },
 });
 
 export const axiosDashboardInstance = axios.create({
   baseURL: `${SERVER_DOMAIN}/api/v1/dashboard`, // Backend server URL
   withCredentials: true, // To send cookies with requests
+  headers: {
+    Cookie: cookieHeader,
+  },
 });
